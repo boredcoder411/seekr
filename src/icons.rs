54beca6cbf439237e938cd792c9c6fa3f39c6180
luxluth <delphin.blehoussi93@gmail.com>
2024-12-18 11:04:54 +0100

    Loading more icons

--- src/icons.rs
+++ src/icons.rs
@@ -1,13 +1,39 @@
-use crate::resources;
-use gtk::gio;
+use std::io::Read;
 
-pub fn get_icon(name: &str) -> gio::Icon {
+use crate::resources;
+use gtk::{
+    gdk_pixbuf::Pixbuf,
+    gio::{self, Icon},
+};
+
+pub fn load_image_resource(content: &[u8], size: (i32, i32)) -> Icon {
+    let cursor = gtk::gio::MemoryInputStream::from_bytes(&gtk::glib::Bytes::from(content));
+    Icon::from(
+        Pixbuf::from_stream_at_scale(
+            &cursor,
+            size.0,
+            size.1,
+            true,
+            None::<&gtk::gio::Cancellable>,
+        )
+        .unwrap(),
+    )
+}
+
+pub fn get_icon(name_or_path: &str) -> Icon {
     let mut res = resources::ICON_MAP.get().write().unwrap();
-    if let Some(e) = res.get(name) {
+    if let Some(e) = res.get(name_or_path) {
         return e.clone();
     } else {
-        let icon = gio::Icon::from(gio::ThemedIcon::from_names(&[name]));
-        res.insert(name.to_string(), icon.clone());
+        if std::path::Path::new(name_or_path).exists() {
+            if let Ok(mut f) = std::fs::File::open(std::path::Path::new(name_or_path)) {
+                let mut buf = vec![];
+                let _ = f.read_to_end(&mut buf);
+                return load_image_resource(&buf, (512, 512));
+            }
+        }
+        let icon = Icon::from(gio::ThemedIcon::from_names(&[name_or_path]));
+        res.insert(name_or_path.to_string(), icon.clone());
         return icon;
     }
 }
